6dff8e75cb204263e69601e15c8be2447f59cbdc
brenoac76 <breno.ac@gmail.com>
2025-07-19 11:00:28 -0300

    Commit Inicial

---
+++ src/main/java/br/com/brenoandrade/ProjetoModulo40/ProjetoModulo40Application.java
@@ -0,0 +1,13 @@
+package br.com.brenoandrade.ProjetoModulo40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoModulo40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoModulo40Application.class, args);
+	}
+
+}
